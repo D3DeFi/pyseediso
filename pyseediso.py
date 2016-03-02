336698100ca4c0c27aaf35900e1e2c4bd915da1c
Dusan Matejka <matejkadusan32@gmail.com>
2016-03-02 15:19:14 +0100

    implemented network parsing

--- pyseediso.py
+++ pyseediso.py
@@ -3,15 +3,26 @@
 import argparse
 from defaults import Defaults
 from jinja2 import Environment, PackageLoader
+from netaddr import IPNetwork, AddrFormatError
 
-TODO = """
-   d-i partman-basicfilesystems/no_swap boolean true if no swap is detected within primary or lvm options
-"""
 
 class Config(Defaults):
 
     def __init__(self, *args, **kwargs):
         super(Config, self).__init__(*args, **kwargs)
+        self.network = parse_network_string(self.network)
+
+    def update(self, item, value):
+        if hasattr(self, item):
+            setattr(self, item, value)
+
+
+def parse_network_string(netstr):
+    try:
+        ip = IPNetwork(netstr)
+        return [ip.ip, ip.netmask]
+    except AddrFormatError:
+        return ['']*2
 
 
 if __name__ == '__main__':
@@ -23,8 +34,8 @@ if __name__ == '__main__':
     arg_parser.add_argument('--hostname', help='hostname for installed system')
     arg_parser.add_argument('--domain', help='domain for installed system')
     arg_parser.add_argument(
-        '--network', help='IP and prefix to be used during system install eg --network 10.0.0.2/24')
-    arg_parser.add_argument('--gateway', help='gateway IP to be used during system install')
+        '--net', help='IP and prefix to be used during system install eg --network 10.0.0.2/24')
+    arg_parser.add_argument('--gw', help='gateway IP to be used during system install')
     arg_parser.add_argument(
         '--dns', help="nameservers for installed system eg --dns '8.8.8.8 8.8.4.4'")
     arg_parser.add_argument(
@@ -45,4 +56,30 @@ if __name__ == '__main__':
     env = Environment(loader=PackageLoader('pyseediso', 'templates'))
     template = env.get_template('preseed.j2')
     config = Config()
+
+    def update_attr(obj, args):
+        attributes = {
+            'hostname': args.hostname,
+            'domain': args.domain,
+            'disks': args.disks,
+            'grub': args.grub,
+            'poweroff': args.poweroff}
+
+        for key, value in attributes.iteritems():
+            if value:
+                obj.update(key, value) 
+
+    update_attr(config, args)
+
+    if bool(args.net) ^ bool(args.gw):
+        arg_parser.error('--net and --gw options must be used together')
+    elif args.net and args.gw:
+        config.update('configure_networking', 'true')
+        config.update('disable_autoconfig', 'true')
+        config.update('network', parse_network_string(args.net))
+        config.update('gateway', args.gw)
+
+    if args.dns:
+        config.update('dns', args.dns)
+
     print template.render(config=config)
