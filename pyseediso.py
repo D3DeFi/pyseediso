a75a11458a438f977e4d9993ed3e9f3991753b54
Dusan Matejka <matejkadusan32@gmail.com>
2016-03-03 10:33:14 +0100

    enhanced argument parsing

--- pyseediso.py
+++ pyseediso.py
@@ -26,60 +26,56 @@ def parse_network_string(netstr):
 
 
 if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description='Generates Debian preseed configuration and attaches it to the specified ISO image.',
         formatter_class=argparse.RawTextHelpFormatter)
-    arg_parser.add_argument('INPUT_ISO', help='specify which ISO to preseed')
-    arg_parser.add_argument('-o', help='name of a newly preseeded ISO', metavar='OUTPUT_ISO')
-    arg_parser.add_argument('--hostname', help='hostname for installed system')
-    arg_parser.add_argument('--domain', help='domain for installed system')
-    arg_parser.add_argument(
-        '--net', help='IP and prefix to be used during system install eg --network 10.0.0.2/24')
-    arg_parser.add_argument('--gw', help='gateway IP to be used during system install')
-    arg_parser.add_argument(
-        '--dns', help="nameservers for installed system eg --dns '8.8.8.8 8.8.4.4'")
-    arg_parser.add_argument(
-        '--disks', help="disks, which should be used for install eg --disks '/dev/sda /dev/sdb'")
-    arg_parser.add_argument('--raid', help='if software raid should be used and what type', choices=[0, 1, 10])
-    arg_parser.add_argument(
-        '--primary', 
-        help="list of primary partitions to create eg --primary '/boot 500M ext4; \
-/data 5G xfs'\ndo not specify LVM as primary partition, use --lvm option instead")
-    arg_parser.add_argument(
-        '--lvm', 
-        help="list of LVs to create eg --lvm '/ 2G ext4; /var 3G xfs; /home 1G xfs'")
-    arg_parser.add_argument('--grub', help="where to install GRUB eg --grub '/dev/sda'")
-    arg_parser.add_argument(
-        '--poweroff', help='shutdown system after installation instead of restarting it', action='store_true')
-    args = arg_parser.parse_args()
-
-    env = Environment(loader=PackageLoader('pyseediso', 'templates'))
-    template = env.get_template('preseed.j2')
-    config = Config()
+    parser.add_argument('INPUT_ISO', metavar='<input_iso>', help='input ISO')
+    parser.add_argument('-o', metavar='<output_iso>', default='pyseed.iso', dest='output', help='output ISO')
+    parser.add_argument('--preseed', metavar='<file>', default='', help='complete preseed to attach to ISO')
+    parser.add_argument('--gen-only', metavar='<file>', default='', help='only generate preseed file')
+    netopt = parser.add_argument_group('network arguments')
+    netopt.add_argument('--name', metavar='<hostname>', help='system hostname')
+    netopt.add_argument('--domain', metavar='<domain>', help='system domain')
+    netopt.add_argument('--net', metavar='<network>', help='IP/prefix (ex. --net 10.0.0.2/24)')
+    netopt.add_argument('--gw', metavar='<gateway>', help='gateway IP')
+    netopt.add_argument('--dns', metavar='<nameservers>', help="nameservers (ex. --dns '8.8.8.8 8.8.4.4')")
+    diskopt = parser.add_argument_group('disk arguments')
+    diskopt.add_argument('--disks', metavar='<disks>', help="install disks (ex. --disks '/dev/sda /dev/sdb')")
+    diskopt.add_argument('--raid', metavar='<raid-type>', choices=[0, 1, 10], help='software raid (ex. --raid=1)')
+    diskopt.add_argument('--primary', metavar='<part>', help="primary partitions (ex. --primary '/boot 500M ext4')")
+    diskopt.add_argument('--lvm', metavar='<lvs>', help="logical volumes (ex. --lvm '/ 2G ext4; /var 3G xfs;')")
+    diskopt.add_argument('--grub', metavar='<device>', help="GRUB destination (ex. --grub '/dev/sda /dev/sdb')")
+    parser.add_argument('--poweroff', help='shutdown machine after install', action='store_true')
+    args = parser.parse_args()
 
     def update_attr(obj, args):
         attributes = {
-            'hostname': args.hostname,
+            'hostname': args.name,
             'domain': args.domain,
             'disks': args.disks,
             'grub': args.grub,
-            'poweroff': args.poweroff}
-
+            'poweroff': args.poweroff
+        }
         for key, value in attributes.iteritems():
             if value:
                 obj.update(key, value) 
 
-    update_attr(config, args)
+    if not args.preseed:
+        env = Environment(loader=PackageLoader('pyseediso', 'templates'))
+        template = env.get_template('preseed.j2')
+        config = Config()
 
-    if bool(args.net) ^ bool(args.gw):
-        arg_parser.error('--net and --gw options must be used together')
-    elif args.net and args.gw:
-        config.update('configure_networking', 'true')
-        config.update('disable_autoconfig', 'true')
-        config.update('network', parse_network_string(args.net))
-        config.update('gateway', args.gw)
+        update_attr(config, args)
 
-    if args.dns:
-        config.update('dns', args.dns)
+        if bool(args.net) ^ bool(args.gw):
+            parser.error('--net and --gw options must be used together')
+        elif args.net and args.gw:
+            config.update('configure_networking', 'true')
+            config.update('disable_autoconfig', 'true')
+            config.update('network', parse_network_string(args.net))
+            config.update('gateway', args.gw)
 
-    print template.render(config=config)
+        if args.dns:
+            config.update('dns', args.dns)
+
+        print template.render(config=config)
